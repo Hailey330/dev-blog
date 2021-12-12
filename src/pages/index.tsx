c0cd8bcddfbb7e9a8abd8c6bc13d5f4d96c48704
Hailey330 <minkky330@gmail.com>
2021-12-12 20:57:22 +0900

    Feat: 간단하게 페이지 띄워보기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import Text from 'components/Text'
+import { Link } from 'gatsby'
 import React, { FunctionComponent } from 'react'
 
 const IndexPage: FunctionComponent = function () {
-  return <Text text="Home" />
+  return (
+    <div>
+      <Text text="Hello, World!" />
+      <Link to="/info/">To Info</Link>
+    </div>
+  )
 }
 
 export default IndexPage
