1cf4fd570db5ebafbc4fddc68774ebd2cb5a0189
Hailey330 <minkky330@gmail.com>
2021-12-29 00:44:08 +0900

    분할된 포스트 목록 받아 출력하기

--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -1,14 +1,41 @@
-import React, { MutableRefObject, useRef } from 'react'
+import { MutableRefObject, useMemo, useRef, useState } from 'react'
 import { PostListItemType } from 'types/PostItem.types'
+
+export type useInfiniteScrollType = {
+  containerRef: MutableRefObject<HTMLDivElement | null>
+  postList: PostListItemType[]
+}
+
+const NUMBER_OF_ITEMS_PER_PAGE = 10
 
 const useInfiniteScroll = (
   selectedCategory: string,
   posts: PostListItemType[],
-) => {
+): useInfiniteScrollType => {
   const containerRef: MutableRefObject<HTMLDivElement | null> =
     useRef<HTMLDivElement>(null)
 
-  return <div></div>
+  const [count, setCount] = useState<number>(1)
+
+  const postListByCategory = useMemo(
+    () =>
+      posts.filter(
+        ({
+          node: {
+            frontmatter: { categories },
+          },
+        }: PostListItemType) =>
+          selectedCategory !== 'All'
+            ? categories.includes(selectedCategory)
+            : true,
+      ),
+    [selectedCategory],
+  )
+
+  return {
+    containerRef,
+    postList: postListByCategory.slice(0, count * NUMBER_OF_ITEMS_PER_PAGE),
+  }
 }
 
 export default useInfiniteScroll
