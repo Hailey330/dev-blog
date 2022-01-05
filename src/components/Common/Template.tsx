50a3a9984f28ca18d7b48aa107a648b274795feb
Hailey330 <minkky330@gmail.com>
2022-01-05 12:49:05 +0900

    Template 컴포넌트에서 Meta 태그 작성하기

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -2,8 +2,13 @@ import React, { FunctionComponent, ReactNode } from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from './GlobalStyle'
 import Footer from './Footer'
+import { Helmet } from 'react-helmet'
 
 type TemplateProps = {
+  title: string
+  description: string
+  url: string
+  image: string
   children: ReactNode
 }
 
@@ -13,9 +18,37 @@ const Container = styled.div`
   height: 100%;
 `
 
-const Template: FunctionComponent<TemplateProps> = ({ children }) => {
+const Template: FunctionComponent<TemplateProps> = ({
+  title,
+  description,
+  url,
+  image,
+  children,
+}) => {
   return (
     <Container>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:url" content={url} />
+        <meta property="og:site_name" content={title} />
+
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={image} />
+        <meta property="twitter:site" content="@사용자이름" />
+        <meta property="twitter:creator" content="@사용자이름" />
+      </Helmet>
+
       <GlobalStyle />
       {children}
       <Footer />
